5d47283e993258945ea88bea15617417cc985b25
farhan687 <farhan687@gmail.com>
2016-11-15 10:33:19 +0530

    Fix travis

--- build.sh
+++ build.sh
@@ -10,17 +10,17 @@ set -e
 
 python generate.py
 
-#clone `master' branch of the repository using encrypted GH_TOKEN for authentification
-git clone https://${GH_TOKEN}@github.com/appbaseio/appbaseio.github.io.git ../appbaseio.github.io.master
+#clone `master' branch of the repository using encrypted GITHUB_SECRET_TOKEN for authentification
+git clone https://${GITHUB_SECRET_TOKEN}@github.com/appbaseio/appbaseio.github.io.git ../appbaseio.github.io.master
 
 # copy generated HTML site to `master' branch
-cp -R assets/* repo_images/* images/* index.html ../appbaseio.github.io.master
+cp -R ./index.html ./repos.json ./assets/* ./repo_images/* ./bower_components/* ./images/* ../appbaseio.github.io.master
 
 # commit and push generated content to `master' branch
 # since repository was cloned in write mode with token auth - we can push there
 cd ../appbaseio.github.io.master
 git config user.email "appbaseio@gmail.com"
-git config user.name "Farhan Chauhan"
+git config user.name "Appbase bot"
 git add -A .
-git commit -a -m "Travis #$TRAVIS_BUILD_NUMBER"
-git push --quiet origin master > /dev/null 2>&1
+git commit -a -m "Travis Build #$TRAVIS_BUILD_NUMBER"
+git push https://${GITHUB_SECRET_TOKEN}@github.com/appbaseio/appbaseio.github.io.git master:master > /dev/null 2>&1
